a7b42f70128b7d8a17a93c57171cc5529c38061f
Chad Reynolds <chad@chadreynolds.info>
2017-07-31 13:24:09 -0500

    Clean up code and add logging messages

--- client.py
+++ client.py
@@ -1,42 +1,61 @@
 from threading import Thread
 from socket import socket
-import time
+from time import sleep
+import logging as log
 
 
 class Client(object):
 
-  SERVER_ADDRESS = ("localhost", 36000)
+    SERVER_ADDRESS = ("localhost", 36000)
 
-  def __init__(self, host, listenport, sendport):
-    self.listen_socket = self.setup_socket((host, listenport))
-    self.send_socket = self.setup_socket((host, sendport))
-    self.listen_thread = Thread(target = self.listen)
-    self.send_thread = Thread(target = self.send)
-    self.message_queue = []
+    def __init__(self, host, receive_port, transmit_port):
+        self.host = host
+        self.receive_port = receive_port
+        self.transmit_port = transmit_port
+        self.message_queue = []
 
-  def listen(self):
-    while True:
-      message_from_server = self.listen_socket.recv(4096)
-      print("\n" + message_from_server.decode().strip() + "\n>>>")
+        self.receiver = self.create_socket((host, receive_port))
+        log.info("Client receiver is connected.")
+        self.transmitter = self.create_socket((host, transmit_port))
+        log.info("Client transmitter is connected.")
 
-  def send(self):
-    while True:
-      if len(self.message_queue) > 0:
-        self.send_socket.sendall(self.message_queue.pop(0))
-      else:
-        time.sleep(1)
+        self.running = False
+        self.receiver_thread = create_thread(self.receive)
+        self.transmitter_thread = create_thread(self.transmit)
+        log.info("Client created with host({}), receive_port({}), transmit_port({})".format(self.host, self.receive_port, self.transmit_port))
 
-  def running(self):
-    self.listen_thread.start()
-    self.send_thread.start()
+    def start_client(self): 
+        self.running = True
+        self.listen_thread.start()
+        self.send_thread.start()
 
-    while True:
-      message = input(">>>")
-      self.message_queue.append(bytes(message, "utf-8"))
+        log.info("Client started.")
+        while self.running:
+            message = input(">>>")
+            self.message_queue.append(bytes(message, "utf-8"))
+            log.debug("New message added to queue: {}".format(message))
 
-  def setup_socket(self, address):
-    s = socket()
-    s.bind(address)
-    s.connect(self.SERVER_ADDRESS)
-    return s
+    def receive(self):
+        while self.running:
+            message = self.listen_socket.recv(4096)
+            message = message.decode()
+            log.debug("New message received from server: {}".format(message))
+            print("\n" + message + "\n>>>")
+
+    def transmit(self):
+        while self.running:
+            if len(self.message_queue) > 0:
+                log.info("Message sent to server.")
+                self.send_socket.sendall(self.message_queue.pop(0))
+            else:
+                sleep(1)
+
+    def create_socket(self, address):
+        s = socket()
+        s.bind(address)
+        s.connect(Client.SERVER_ADDRESS)
+        return s
+
+    def create_thread(self, target_func):
+        return Thread(target = target_func)
 
--- server.py
+++ server.py
@@ -1,61 +1,93 @@
 from threading import Thread
 from socket import socket
-import time
-
+from time import sleep
+import logging as log
 
 
 class Server(object):
-  def __init__(self, port = 36000):
-    self.port = port
-    self.clients = dict()
-    self.messages = []
-    self.listening_socket = self.set_up_listening_socket(self.port)
+    
+    def __init__(self, port = 36000):
+        self.port = port
+        self.clients = dict()
+        self.listener = self.create_listen_socket(("localhost", self.port))
 
-    self.listen_thread = Thread(target = self.listen)
-    self.relay_thread = Thread(target = self.relay_messages)
+        self.running = False
+        self.receiver_thread = create_thread(self.listen)
+        self.relay_thread = create_thread(self.relay_messages)
 
-  def start_server(self):
-      self.listen_thread.run()
-      self.relay_thread.run()
+        log.info("Server created with port {}.".format(self.port))
 
-  def relay_messages(self):
-    while True:
+    def start_server(self):
+        self.running = True
+        self.receiver_thread.start()
+        self.relay_thread.start()
+
+        log.info("Server started.")
+
+    def relay_messages(self):
+        log.info("Message relaying started.")
+        while self.running:
+             for address, client in self.clients.items():
+                if client.complete:
+                    try:
+                        message = client.get_message_from()
+                        log.debug("Received message from {}: {}".format(address, message))
+                        self.relay_message(message, address)
+
+                    except BlockingIOError:
+                        log.debug("No message from {}.".format(address))
+
+            sleep(1)
+
+    
+
+    def relay_message(self, message, sender):
         for address, client in self.clients.items():
-            try:
-                msg = client.sending_socket.recv(4096)
-                self.relay_message(msg, address)
-            except BlockingIOError:
-                pass
-        time.sleep(1)
+            if sender != address:
+                client.send_message_to(message)
+                log.debug("Sent message to {}.".format(address))
 
+    def listen(self):
+        log.info("Connection listening started.")
+        while self.running:
+            connection = self.listener.accept()
+            log.info("New Connection.")
+            new_socket, address = (connection[0], connection[1])
 
-  def relay_message(self, msg, sender):
-    for address, client in self.clients.items():
-      if sender != address:
-        client.listening_socket.sendall(msg)
+            if address not in self.clients:
+                self.clients[address] = ClientConnection(address, new_socket)
+                log.debug("Connection is new client at address {}.".format(address))
 
-  def listen(self):
-    while True:
-      connection = self.listening_socket.accept()
-      new_socket = connection[0]
-      address = connection[1]
-      if address in self.clients:
-        new_socket.setblocking(False)
-        self.clients[address].sending_socket = new_socket
-      else:
-        self.clients[address] = ClientConnection(address, new_socket)
+            else:
+                new_socket.setblocking(False)
+                self.clients[address].client_transmitter(new_socket)
+                self.clients[address].complete = True
+                log.debug("Connection is existing client at address {}.".format(address))
 
-  def set_up_listening_socket(self, port):
-    s = socket()
-    s.bind(("localhost", port))
-    s.listen()
-    return s
-      
-
+    def create_listen_socket(self, address):
+        s = socket()
+        s.bind(address)
+        s.listen()
+        return s
+    
+    def create_thread(self, target_func):
+        return Thread(target = target_func)
+    
 
 class ClientConnection(object):
-  def __init__(self, address, listening_socket, sending_socket = None):
-    self.address = address
-    self.listening_socket = listening_socket
-    self.sending_socket = sending_socket
+
+    MESSAGE_BUFFER_SIZE = 4096
+
+    def __init__(self, address, receiver_socket, transmitter_socket=None):
+        self.address = address
+        self.client_receiver = receiver_socket
+        self.client_transmitter = transmitter_socket
+        self.complete = False
+
+    def get_message_from(self):
+        return self.client_transmitter.recv(ClientConnection.MESSAGE_BUFFER_SIZE)
+
+    def send_message_to(self, message):
+        self.client_receiver.sendall(message)
+
 
